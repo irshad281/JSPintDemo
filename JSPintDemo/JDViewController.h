598ddba82e958b97a49eb9c8d2d7d630e7cb272c
Jay Slupesky <jay.slupesky@wdc.com>
2012-10-07 16:20:03 -0700

    First Commit

--- JSPintDemo/JDViewController.h
+++ JSPintDemo/JDViewController.h
@@ -2,12 +2,14 @@
 //  JDViewController.h
 //  JSPintDemo
 //
-//  Created by Jay Slupesky on 10/6/12.
+//  Created by Jay Slupesky on 10/7/12.
 //  Copyright (c) 2012 Fall Creek Software. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "PintCollectionViewLayout.h"
+#import "PNCollectionCellBackgroundView.h"
 
-@interface JDViewController : UIViewController
+@interface JDViewController : UIViewController <UICollectionViewDelegateJSPintLayout,UICollectionViewDataSource>
 
 @end
---
+++ JSPintDemo/PNCollectionCellBackgroundView.h
@@ -0,0 +1,12 @@
+//
+//  PNCollectionCellBackgroundView.h
+//
+//  Created by Jay Slupesky on 10/3/12.
+//  Copyright (c) 2012 Fall Creek Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNCollectionCellBackgroundView : UIView
+
+@end
