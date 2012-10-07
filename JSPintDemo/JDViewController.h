1755627b30e67b42ac60091fd86340b56808a65f
Jay Slupesky <jay.slupesky@wdc.com>
2012-10-06 21:24:21 -0700

    Initial Commit

---
+++ JSPintDemo/JDViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDViewController.h
+//  JSPintDemo
+//
+//  Created by Jay Slupesky on 10/6/12.
+//  Copyright (c) 2012 Fall Creek Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDViewController : UIViewController
+
+@end
---
+++ JSPintDemo/JDViewController.m
@@ -0,0 +1,29 @@
+//
+//  JDViewController.m
+//  JSPintDemo
+//
+//  Created by Jay Slupesky on 10/6/12.
+//  Copyright (c) 2012 Fall Creek Software. All rights reserved.
+//
+
+#import "JDViewController.h"
+
+@interface JDViewController ()
+
+@end
+
+@implementation JDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
